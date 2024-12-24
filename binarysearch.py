cc3ff8af812d8db8f2c97e2882d4084c672da8cb
ikgagan <ik.gagan@gmail.com>
2024-12-24 01:45:04 -0800

    3 questions added

---
+++ binarysearch.py
@@ -0,0 +1,8 @@
+# Question: Binary Search
+# Description:
+# Given a sorted array of integers nums and a target value, return the index of the target if it exists. If not, return -1.
+
+# Example:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: The target 9 is at index 4.
---
+++ removedulplicates.py
@@ -0,0 +1,7 @@
+# Question: Remove Duplicates from Sorted Array
+# Description:
+# Given an integer array nums sorted in non-decreasing order, remove duplicates in-place so that each unique element appears only once. Return k as the number of unique elements.
+
+# Example:
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]

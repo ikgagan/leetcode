17b6449338c711d8caafecff8dccdef611486a89
ikgagan <ik.gagan@gmail.com>
2024-12-24 16:35:29 -0800

    question added

---
+++ besttimetobuystocks.py
@@ -0,0 +1,9 @@
+# Question : Best Time to Buy and Sell Stock
+# Description:
+# You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+# Find the maximum profit you can achieve. You may complete at most one transaction.
+
+# Example:
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.

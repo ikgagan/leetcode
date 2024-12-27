7a108e8e7b86ef4f783c84d336ebbbf3a1b38761
ikgagan <ik.gagan@gmail.com>
2024-12-27 01:35:46 -0800

    power of 2 question

---
+++ powerof2.py
@@ -0,0 +1,7 @@
+#Question:  Power of Two
+# Problem:
+# Given an integer n, return true if it is a power of two. Otherwise, return false.
+# Example:
+# Input: n = 16
+# Output: true
+# Explanation: 2^4 =16

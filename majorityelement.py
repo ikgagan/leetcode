9ffd606fc8e23b20dc1fde846a97cb5362ad5c01
ikgagan <ik.gagan@gmail.com>
2024-12-27 01:31:13 -0800

    majority element question

---
+++ majorityelement.py
@@ -0,0 +1,17 @@
+# Question :  Majority Element
+# Problem:
+# Given an array nums of size 
+# 𝑛
+# n, return the majority element.
+# The majority element is the element that appears more than 
+# ⌊
+# 𝑛
+# /
+# 2
+# ⌋
+# ⌊n/2⌋ times.
+# Assume the array is non-empty and a majority element always exists.
+
+# Example:
+# Input: nums = [3,2,3]
+# Output: 3

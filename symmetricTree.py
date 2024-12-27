a1cf2e6857fef8abdb7ecbe938f9e8320e54cf49
ikgagan <ik.gagan@gmail.com>
2024-12-27 01:36:50 -0800

    Symmetric Tree question

---
+++ symmetricTree.py
@@ -0,0 +1,7 @@
+# Question: Symmetric Tree
+# Problem:
+# Given the root of a binary tree, check whether it is symmetric around its center.
+
+# Example:
+# Input: root = [1,2,2,3,4,4,3]
+# Output: true

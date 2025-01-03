680fa141ae3643737d9ccd1eab2220e8f2409bb9
ikgagan <ik.gagan@gmail.com>
2025-01-02 21:01:27 -0800

    power of 2

--- powerof2.py
+++ powerof2.py
@@ -5,3 +5,18 @@
 # Input: n = 16
 # Output: true
 # Explanation: 2^4 =16
+
+
+def isPowerOfTwo(n: int) -> bool:
+    if n <= 0:
+        return False
+    while n % 2 == 0:
+        n //= 2
+    return n == 1
+
+# Test cases
+print(isPowerOfTwo(16))  # Output: True
+print(isPowerOfTwo(18))  # Output: False
+print(isPowerOfTwo(1))   # Output: True
+print(isPowerOfTwo(0))   # Output: False
+print(isPowerOfTwo(-2))  # Output: False

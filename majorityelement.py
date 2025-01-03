89d1cc1e275a6e0d570496b0af734593e68dea1d
ikgagan <ik.gagan@gmail.com>
2025-01-02 21:19:23 -0800

    majority element solution

--- majorityelement.py
+++ majorityelement.py
@@ -15,3 +15,26 @@
 # Example:
 # Input: nums = [3,2,3]
 # Output: 3
+
+def majorityElement(nums: list[int]) -> int:
+  # Boyer-Moore Algorithm
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif num == candidate:
+            count += 1
+        else:
+            count -= 1
+
+    return candidate
+
+
+nums = [3, 2, 3]
+print(majorityElement(nums))  # Output: 3
+
+nums = [2, 2, 1, 1, 1, 2, 2]
+print(majorityElement(nums))  # Output: 2
